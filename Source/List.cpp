2df52b2b87d241fa8f526507434e96162e1bfaef
Mikele <smeshmike@yandex,ru>
2020-12-04 14:02:12 +0300

    fix

--- Source/List.cpp
+++ Source/List.cpp
@@ -6,16 +6,18 @@ list<int>::list()
 {
 	this->size = 0;
 	this->head = new node<int>;
+	head->next = this->tail;
 	this->tail = head;
+	this->tail->prev = head;
+	this->tail->value = '\0';
 }
 
 void list<int>::Insert(int data)
 {
 	node<int>* nnd = new node<int>;
 	nnd->value = data;
+	nnd->prev = this->tail;
 	this->size++;
+	this->tail = nnd;
 
-	tail = tail.
-	
-	this->size++;
 }
